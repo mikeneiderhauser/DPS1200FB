c0c489c5d31c0a896420d42d419d15836132c8a0
Mike Neiderhauser <mikeneiderhauser@gmail.com>
2020-09-15 15:59:26 -0400

    Clean code to only work with ATTINY85 Upload attiny85_firmware.hex and remove old firmware Update README for avrdude cmds and shoutout

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # DPS1200FB
 Basic fan control for DPS1200FB or similar power supplies
+
+# Shoutout
+Many thanks to the work done in this repo a few years ago https://github.com/raplin/DPS-1200FB
+
+# AVRDUDE Commands
+avrdude -c usbtiny -P usb -p attiny85
+
+avrdude -c usbtiny -P usb -p attiny85 -e -U lfuse:w:0xE2:m -U hfuse:w:0xDF:m -U efuse:w:0xFF:m -U lock:w:0xFF:m
+
+avrdude -c usbtiny -P usb -p attiny85 -e -U flash:w:firmware.hex:a
+
+
--- Sketch/DPS1200FB/DPS1200FB.ino
+++ Sketch/DPS1200FB/DPS1200FB.ino
@@ -1,11 +1,13 @@
-#define TINY  // Specify build for attiny
+// Include TinyWireM Lib for ATTINY I2C over USI
+#include <TinyWireM.h>
+#include <USI_TWI_Master.h>
 
 /*
  * ATTINY85 Wiring
  *       ----------
  * /RST--|1      8|--  VCC
  * A3  --|2      7|--  SCL
- * A2  --|3      6|--  1 (NC)
+ *  2  --|3      6|--  1 - LED
  * GND --|4      5|--  SDA
  *       ----------
  * 
@@ -24,12 +26,12 @@
  * See Repo for schematic
  * 
  * ATTINY PINS
- * 1 - /RST -> Pulled high w/ 10K Resistor
+ * 1 - /RST - Pulled high w/ 10K Resistor
  * 2 - A3 - to 3-pin jumper for analog pot
- * 3 - A2 - Pulled high internally (via code INPUT_PULLUP) - Jump to gnd to disable A3
+ * 3 -  2 - Pulled high internally (via code INPUT_PULLUP) - Jump to gnd to disable A3
  * 4 - GND
  * 5 - SDA - Connected to SDA Pin 32 on PSU
- * 6 - Digital Pin 1 - NC
+ * 6 -  1  - LED with 1K resistor to GND
  * 7 - SCL - Connected to SCL Pin 31 on PSU
  * 8 - VCC - 5V from reg
  * 
@@ -48,30 +50,14 @@
  * 38 - PSU Alarm (NC)
  */
 
-#ifdef TINY
-/* https://github.com/lucullusTheOnly/TinyWire */
-// Add as zip lib
-#include <twi.h>
-#include <TinyWire.h>
-#else
-#include <Wire.h>
-#endif
-
-/* Enable Serial to print debug messages */
-#ifndef TINY
-  #define SERIALEN
-#endif
-
-/* Enable I2C - Enable / Disable messages going to PSU - useful for debugging */
-#define I2CEN
 /* Address of PIC MCU in PSU (0x57 -> EEPROM, 0x5F -> PIC MCU) - DOESN'T ACCOUNT FOR ADDRESS SELECT PINS */
 #define I2CADDR 0x5F 
-
 #define STATIC_RPM 8000
 
 /* Analog Control - adjust PSU RPM Fan via Analog input - uses map function */
 #define AIN_PIN   A3  // Works for both ATTINY85 and Pro Micro
-#define ACTRL_PIN A2  // Works for both ATTINY85 and Pro Micro
+#define LED_PIN 1     // Pin for blink code LED
+#define ACTRL_PIN  2  // Works for both ATTINY85 and Pro Micro
 
 /* Map values for analog control */
 #define MAP_INIT_LOW 0 
@@ -79,10 +65,9 @@
 #define MAP_END_LOW 3300
 #define MAP_END_HIGH 16000
 
+/* Common Delays */
 #define LOOP_DELAY 1000
-
-// analog input val
-uint16_t ain_val = MAP_INIT_LOW;
+#define BLINK_TIME 100
 
 // vals for i2c write
 uint8_t  reg = 0x40;
@@ -94,75 +79,73 @@ uint8_t  regCS = 0;
 uint8_t  data[4] = {0,0,0,0};
 uint8_t  analog_ctrl_state = HIGH;
 
-void setup() {
-  // put your setup code here, to run once:
-  #ifdef I2CEN
-    #ifndef TINY
-      Wire.begin();
-    #else
-      TinyWire.begin();
-    #endif
-  #endif
+#define ENBLINKCODES // Enable / disable led blink codes.
 
-  pinMode(AIN_PIN, INPUT);
-  pinMode(ACTRL_PIN, INPUT_PULLUP);
-  analog_ctrl_state = digitalRead(ACTRL_PIN);
-
-  #ifdef SERIALEN
-    Serial.begin(9600);
-    while (!Serial); // Leonardo: wait for serial monitor
+void blinkCode(uint8_t ct){
+  #ifdef ENBLINKCODES
+  for(uint8_t i = 0; i<ct; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(BLINK_TIME);
+    digitalWrite(LED_PIN, LOW);
+    delay(BLINK_TIME);
+  }
+  // end blink timeout
+  delay(LOOP_DELAY);
   #endif
 }
 
-void loop() {
+void setup() {
+  pinMode(LED_PIN, OUTPUT);          // LED Pin as output for blink codes
+  digitalWrite(LED_PIN, LOW);        // Initial state of LED is off
+  pinMode(AIN_PIN, INPUT);           // Analog input pin for potentiometer 
+  pinMode(ACTRL_PIN, INPUT_PULLUP);  // Control Pin -> Jumper on for static RPM, Jumper off for potentiometer ctrl
+
+  // Start I2C
+  TinyWireM.begin();
+
+  // read control state
   analog_ctrl_state = digitalRead(ACTRL_PIN);
+}
+
+void loop() {
+  blinkCode(1);
+
+  // re-read control state
+  analog_ctrl_state = digitalRead(ACTRL_PIN);
+  
   if(analog_ctrl_state == HIGH) {
     // Jumper not installed
-    // read analog value
-    ain_val = analogRead(AIN_PIN);
-    rpm_value = map(ain_val, MAP_INIT_LOW, MAP_INIT_HIGH, MAP_END_LOW, MAP_END_HIGH);
-  } else {
+    // read analog value and map to RPM
+    rpm_value = map((uint16_t)analogRead(AIN_PIN), MAP_INIT_LOW, MAP_INIT_HIGH, MAP_END_LOW, MAP_END_HIGH);
+  } else { // LOW
     // Jumper installed
     rpm_value = STATIC_RPM;
   } 
   
-  #ifdef SERIALEN
-    Serial.print(ain_val);
-    Serial.print(" : ");
-    Serial.println(rpm_value);
-  #endif
+  // Most of the following is taken from the dump work from this github repo
+  // https://github.com/raplin/DPS-1200FB/blob/master/DPS-1200FB.py
 
-  #ifdef I2CEN
-    // Most of the following is taken from the dump work from this github repo
-    // https://github.com/raplin/DPS-1200FB/blob/master/DPS-1200FB.py
-  
-    // Extract MSB and LSB from RPM
-    valLSB=rpm_value&0xff;
-    valMSB=rpm_value>>8;
-    // calculate checksum - the checksum is the 'secret sauce'
-    cs=(I2CADDR<<1)+reg+valLSB+valMSB;
-    regCS=((0xff-cs)+1)&0xff;
-   
-    // pack the data
-    data[0] = reg;
-    data[1] = valLSB;
-    data[2] = valMSB;
-    data[3] = regCS;
-  
-    // write to psu
-    #ifndef TINY
-      Wire.beginTransmission(I2CADDR);
-      Wire.write(data, 4);
-      Wire.endTransmission();
-    #else
-      TinyWire.beginTransmission(I2CADDR);
-      for(uint8_t i=0; i<4; i++) {
-        TinyWire.send(data[i]);
-      }
-      TinyWire.endTransmission();
-    #endif
-  #endif
+  // Extract MSB and LSB from RPM
+  valLSB=rpm_value&0xff;
+  valMSB=rpm_value>>8;
+  // calculate checksum - the checksum is the 'secret sauce'
+  cs=(I2CADDR<<1)+reg+valLSB+valMSB;
+  regCS=((0xff-cs)+1)&0xff;
+ 
+  // pack the data
+  data[0] = reg;
+  data[1] = valLSB;
+  data[2] = valMSB;
+  data[3] = regCS;
 
-  // basic loop delay - resend i2c message every LOOP_DELAY ms
+  // write to psu
+  blinkCode(2);
+  // I2C Transaction
+  TinyWireM.beginTransmission(I2CADDR);
+  TinyWireM.write(data,4);
+  TinyWireM.endTransmission();
+  blinkCode(3);
+  
+  // basic loop delay - resend i2c message every LOOP_DELAY ms (ignoring time for blinks)
   delay(LOOP_DELAY);
 }
